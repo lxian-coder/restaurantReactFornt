890dd5ae1dff59f6a80572349f563a83650c38c6
darcyxian <darcyxianau@gmail.com>
2021-07-18 23:06:42 +1000

    remove menu description format

--- components/Pages/components/Menu/Menu.tsx
+++ components/Pages/components/Menu/Menu.tsx
@@ -118,9 +118,7 @@ left: 53%;
 z-index: 0;
 `;
 const DesMenu = styled.div`
-    white-space: pre-wrap;
-    word-wrap: break-word;
-    text-align: justify; 
+ 
 `;
 interface menuData{
   id:number,
